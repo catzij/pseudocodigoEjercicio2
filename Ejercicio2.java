798163044541480ea1bcd1565909de9595d8bef6
Josué Casiá <josuecasia201831432@cunoc.edu.gt>
2021-06-09 19:38:13 -0600

    Agregado Ejercicio2.java

---
+++ Ejercicio2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Ejercicio2{
+	public static void main(String args []){
+		Scanner read = new Scanner(System.in);
+
+		int numero;
+		System.out.println("ingrese un numero");
+		numero = read.nextInt();
+
+		while(numero >=1){
+
+ 		System.out.println( numero+"\n");
+	  	
+	  	numero --;
+
+		}
+
+	}
+}
+
+	
